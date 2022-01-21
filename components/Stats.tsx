905de7b6eb3b5a10aca237f3704ba3ecad4f8ff9
Christopher <leyssen@gmail.com>
2022-01-21 08:56:43 +0100

    Dowdall points

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -10,14 +10,21 @@ import PersonIcon from '@mui/icons-material/Person';
 
 import React from "react";
 import Dict = NodeJS.Dict;
+import Typography from "@mui/material/Typography";
 
 const Stats: React.FC<{ tweetId: string }> = ({tweetId}) => {
     const votes = useAllVotes()
+    const v1 = takeVotes(votes, tweetId, 1);
+    const v2 = takeVotes(votes, tweetId, 2);
+    const v3 = takeVotes(votes, tweetId, 3);
+    const points = v1.length + v2.length / 2 + v3.length / 3;
+    if (points === 0) return null
     return (
         <>
-            <Users position={1} emails={takeVotes(votes, tweetId, 1)}/>
-            <Users position={2} emails={takeVotes(votes, tweetId, 2)}/>
-            <Users position={3} emails={takeVotes(votes, tweetId, 3)}/>
+            <Typography>Dowdall points: {points.toFixed(2)}</Typography>
+            <Users position={1} emails={v1}/>
+            <Users position={2} emails={v2}/>
+            <Users position={3} emails={v3}/>
         </>
     )
 };
