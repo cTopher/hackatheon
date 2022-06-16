2def23ec550cbb4cad94793e97ef8e447a3e1cca
Egon Okerman <dev@egon.me>
2022-06-16 17:16:21 +0200

    Change for 22Q2 hackathon

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,25 +38,47 @@ const Home: NextPage = () => {
 const Tweets: React.FC = () => {
     return (
         <>
-            <Tweet id="mounts" name="Michel" avatar="M">
-                On NAB we had phone and tablet mounts to secure our devices on the booth. These mounts we took with us back to the office. For test automation purposes I would like to rebuild this set-up so we have a wall with iPad/iPhone&apos;s connected to our test automation system.
+            <Tweet id="ull-live" name="Daniel" avatar="/daniel.png" img="/mobile-live-streaming.jpg">
+                Today we provide HESP.live SDK for mobile devices to get the streams playing.<br/>
+                <br/>
+                What if we could also do recording and push it directly to HESP.live from our phone? <br/>
+                <br/>
+                I tell you what ... <br/>
+                <br/>
+                Ultra Low-Latency <br/>
+                Youtube/Facebook Live <br/>
+                client is just born.<br/>
             </Tweet>
-            <Tweet id="auto-deploy" name="Michel" avatar="M">
-                In our test automation system we suffer from smart tv&apos;s going offline after a while, result is un-executed tests during the nightly run. During the hackaton would like to investigate if we can develop a script that auto-deploy and turns-on both TIZEN & WebOS smart tv&apos;s.</Tweet>
-            <Tweet id="record" name="Daniel" avatar="/daniel.png" img="/rec.jpg">
-                What if we could also do recording and push it directly to HESP.live from our phone?
-                I tell you what ... Ultra Low-Latency Youtube/Facebook Live client is just born.
+            <Tweet id="hevc-hesp" name="Team Ham" avatar="/team-ham.jpg">
+                HESP is the "High Efficiency Streaming Protocol". Let's make it work together with the "High Efficiency Video Codec" and get some h265 in our HESP. Who knows, maybe we'll be able to cross another bridge and go AV1?
             </Tweet>
-            <Tweet id="quality" avatar="/maxim.png" name="Maxim">
+            <Tweet id="release-button" name="Daniel" avatar="/daniel.png" img="/big-red-button.jpg">
+                Being a release master is not fun.<br/>
+                Let's automate the THEOplayerSDK release process, so my mother could do it too.<br/>
+                <br/>
+                One big physical button, one click and everything is done. <br/>
+                (more info later)<br/>
+            </Tweet>
+            <Tweet id="freemium-theo" name="LeMe33" avatar="/leme33.png">
+                Did you know you get on average 3 USD CPM for each video ad? 27% of the internet users have an ad blocker, but at a 1 USD CPM we normally charge , there would be plenty of headroom to just offer it for free if we play an ad at the start of each video!
+            </Tweet>
+            <Tweet id="mounts" name="Michel" avatar="/michel.png">
+                On NAB we had phone and tablet mounts to secure our devices on the booth. These mounts we took with us back to the office. For test automation purposes I would like to rebuild this set-up so we have a wall with iPad/iPhones connected to our test automation system.
+            </Tweet>
+            <Tweet id="quality" name="Maxim" avatar="/maxim.png">
                 Quality selection for iOS in THEO UI using preferredPeakBitrate/preferredMaximumResolution
             </Tweet>
-            <Tweet id="pi" name="Furby" avatar="/lieven.jpg">
+            <Tweet id="auto-deploy" name="Michel" avatar="/michel.png">
+                In our test automation system we suffer from smart TVs going offline after a while, result is un-executed tests during the nightly run. During the hackaton would like to investigate if we can develop a script that auto-deploy and turns-on both TIZEN &amp; WebOS smart TVs.
+            </Tweet>
+            <Tweet id="pi" name="Lieven" avatar="/lieven.png">
                 Implement an entire HESP encoder and packager stack on a Pi 4B+ or equivalent setup box stack.
             </Tweet>
-            <Tweet id="release" name="Daniel" avatar="/daniel.png">
-                One-click release.
-                Being a release master is not fun. Let&apos;s automate the THEOplayerSDK release process, so my mother could do it too.
-                One big physical button, one click and everything is done.
+            <Tweet id="prerecorded-hesp" name="Michel" avatar="/michel.png">
+                I would like to investigate how we can reduce variations in the video bandwidth, network stability but also reduce the cost/equipment needed to generate a stream. Ideally we would create a function (lambda) that converts a pre-recorded HESP VOD into actual THEOlive.
+            </Tweet>
+            <Tweet id="xagget-on-demand" name="Michel" avatar="/michel.png">
+                Adapt Xagget for internal use. Any person enters the stream url, drm parameters and selects platform ... overnight the test runs and you get a result presented back. Ideal for commercial opportunities.
             </Tweet>
         </>
     )
